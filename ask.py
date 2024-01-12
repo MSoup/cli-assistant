d7d88309ca91f5a22ad24e934182aa008cd61f9d
Daven <davenlu1989@gmail.com>
2024-01-12 18:08:59 +0900

    ServiceFactory dynamic

--- ask.py
+++ ask.py
@@ -9,7 +9,10 @@ import boto3
 
 class Model(ABC):
     """
-    Abstract class that models must implement
+    Abstract class. Models must implement the below
+    :abstractmethod: model()
+    :abstractmethod: invoke()
+    :abstractmethod: add_to_factory()
     """
 
     def __init__(self, model_ver: str):
@@ -19,9 +22,17 @@ class Model(ABC):
     @abstractmethod
     def model(self):
         """
-        Model name
+        Human readable model name
         """
         return self.__model_ver
+
+    @property
+    @abstractmethod
+    def modelId(self):
+        """
+        Model identifier
+        """
+        raise NotImplementedError
 
     @abstractmethod
     def invoke(self, prompt: str):
@@ -30,12 +41,58 @@ class Model(ABC):
         """
         raise NotImplementedError
 
-    # TODO
-    # @abstractmethod
-    # def cost(self, cost_per_1000_tokens: float):
-    #     """
-    #     Implement method to calculate cost of invocation
-    #     """
+    @classmethod
+    @abstractmethod
+    def add_to_factory():
+        """
+        Implement add_to_factory method
+        """
+        raise NotImplementedError
+
+
+class ModelVersion:
+    def __init__(self, model: Model, version: str):
+        self.model = model
+        self.version = version
+
+    def __repr__(self):
+        return f"{self.version}"
+
+
+class ServiceFactory:
+    """
+    Service factory
+    :param str model_name: Human readable model name to initialize
+    :property dict AVAILABLE_MODELS: stores initializable models.\n
+    Models should implement a class, add_to_factory(), that sets ServiceFactory.AVAILABLE_MODELS[ver] to a ModelVersion object
+    Initializes a client for a model
+    """
+
+    AVAILABLE_MODELS = {}
+
+    # AVAILABLE_MODELS: {human_readable_model_name: ModelVersion object}
+    def __init__(self, model_name: str) -> None:
+        self.client = self._get_client(model_name.lower())
+
+    def get_available_models():
+        return f"""Available Models:
+        {list(ServiceFactory.AVAILABLE_MODELS.keys())}
+        {"=" * 35}
+        """
+
+    def invoke(self, prompt) -> None:
+        print(f">>> Invoking {self.client.model} assistant: ")
+        return self.client.invoke(prompt)
+
+    def _get_client(self, model_version: str) -> Model:
+        if model_version in ServiceFactory.AVAILABLE_MODELS:
+            client_params: ModelVersion = ServiceFactory.AVAILABLE_MODELS[model_version]
+            client: Model = client_params.model(model_version)
+        else:
+            raise ValueError(
+                f"Allowed versions: {ServiceFactory.AVAILABLE_MODELS.keys()}"
+            )
+        return client
 
 
 class GPT(Model):
@@ -47,20 +104,29 @@ class GPT(Model):
 
     def __init__(self, model_ver) -> None:
         if not model_ver in self.VERSIONS:
-            raise ValueError("Allowed GPT versions: 3.5, 4")
+            raise ValueError("Allowed versions:", list(self.VERSIONS.items()))
 
-        self.API_KEY = os.environ.get("OPENAI_API_KEY")
         self.model_ver = model_ver
-        self.client = OpenAI(api_key=self.API_KEY)
+        self.api_key = os.environ.get("OPENAI_API_KEY")
+        self.client = OpenAI(api_key=self.api_key)
 
-        super().__init__(model_ver)
+        super().__init__(self.model_ver)
 
     @property
     def model(self):
         return f"GPT-{self.model_ver}"
 
+    @property
+    def modelId(self):
+        return f"{self.VERSIONS[self.model_ver]}"
+
+    @classmethod
+    def add_to_factory(self):
+        for k, v in self.VERSIONS.items():
+            ServiceFactory.AVAILABLE_MODELS[k] = ModelVersion(GPT, v)
+
     def invoke(self, prompt) -> None:
-        if not self.API_KEY:
+        if not self.api_key:
             raise ValueError("Please initialize OPENAI_API_KEY env variable")
 
         completion = self.client.chat.completions.create(
@@ -78,79 +144,46 @@ class GPT(Model):
         return response
 
 
+# https://docs.aws.amazon.com/bedrock/latest/userguide/model-ids-arns.html
 class Claude(Model):
-    """
-    Claude Model by Anthropic. Requires valid ~/.aws/credentials. Model is called through boto3 bedrock-runtime
-    """
+    VERSIONS = {"claude": "anthropic.claude-v2:1"}
 
-    VERSIONS = {"2.1": "anthropic.claude-v2:1"}
-
-    def __init__(self, model_ver) -> None:
-        if not model_ver in self.VERSIONS:
-            raise ValueError("Allowed Claude versions: 2.1")
-
-        self.model_ver = model_ver
-
+    def __init__(self, version: str) -> None:
+        self.version = version
         self.client = boto3.client("bedrock-runtime", region_name="us-east-1")
-
-        super().__init__(self.model_ver)
 
     @property
     def model(self):
-        return f"Claude-{self.model_ver}"
+        return self.version
+
+    @property
+    def modelId(self):
+        return self.VERSIONS[self.version]
+
+    @classmethod
+    def add_to_factory(self):
+        for k, v in self.VERSIONS.items():
+            ServiceFactory.AVAILABLE_MODELS[k] = ModelVersion(Claude, v)
 
     def invoke(self, prompt):
         """
         Invokes the Anthropic Claude 2 model to run an inference using the input
         provided in the request body.
-
         :param prompt: The prompt that you want Claude to complete.
         :return: Inference response from the model.
         """
-
         # Claude requires you to enclose the prompt as follows:
         enclosed_prompt = "Human: " + prompt + "\n\nAssistant:"
-
         body = {
             "prompt": enclosed_prompt,
             "max_tokens_to_sample": 2000,
-            "temperature": 0.5,
+            "temperature": 0.1,
             "stop_sequences": ["\n\nHuman:"],
         }
-
-        response = self.client.invoke_model(
-            modelId=self.VERSIONS[self.model_ver], body=json.dumps(body)
-        )
-
-        response_body = json.loads(response.get("body").read())
+        response = self.client.invoke_model(modelId=self.modelId, body=json.dumps(body))
+        response_body = json.loads(response["body"].read())
         completion = response_body["completion"]
-
         return completion.strip()
-
-
-class ServiceFactory:
-    """
-    Model factory
-    :param str model:
-    """
-
-    def __init__(self, model: str) -> None:
-        self.client = self._get_client(model)
-
-    def invoke(self, prompt) -> None:
-        print(f"{self.client.model} assistant: ")
-        return self.client.invoke(prompt)
-
-    def _get_client(self, model_version) -> Model:
-        if model_version == "3.5":
-            client = GPT("3.5")
-        elif model_version == "4":
-            client = GPT("4")
-        elif model_version.lower() == "claude":
-            client = Claude("2.1")
-        else:
-            raise ValueError("Allowed versions: 3.5, 4, 2.1")
-        return client
 
 
 # WRAPPER
@@ -179,6 +212,16 @@ class ServiceFactory:
 #     return wrapper
 
 if __name__ == "__main__":
+    # Init invokable models
+    GPT.add_to_factory()
+    Claude.add_to_factory()
+
+    # Init env variables
+    load_dotenv()
+
+    # Print available models
+    ServiceFactory.get_available_models()
+
     parser = argparse.ArgumentParser(
         description="Sends a prompt to openai API or Claude, returns a GPT3.5, GPT4 or Claude 2.1 completion response"
     )
@@ -187,15 +230,16 @@ if __name__ == "__main__":
         "-v",
         "--version",
         type=str,
-        default="3.5",
+        default="claude",
         help="Model version (3.5, 4, or claude)'",
         choices=["3.5", "4", "claude"],
     )
     parser.add_argument("prompt_text", type=str, help="Prompt for the model")
 
     args = parser.parse_args()
-    load_dotenv()
 
-    client = ServiceFactory(model=args.version)
+    llm_to_invoke = args.version
+    prompt = args.prompt_text
 
-    print(client.invoke(prompt=args.prompt_text))
+    client = ServiceFactory(llm_to_invoke)
+    print(client.invoke(prompt))
